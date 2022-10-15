2b30a719c9df2fef580507d9cd0210a802d16eba
Priscila <priscilasartori2@gmail.com>
2022-10-15 18:25:12 -0300

    Atualizando arquivos

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -2,14 +2,15 @@ import React from 'react';
 import Title from './Title';
 import missions from '../data/missions';
 import MissionCard from './MissionCard';
+import styles from './Missions.module.css';
 
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions">
+      <div data-testid="missions" className={ styles.missions }>
         <Title headline="Missões" />
         { missions.map(({ name, year, country, destination }) => (
-          <li key={ name }>
+          <li key={ name } className={ styles.missionCard }>
             <MissionCard
               name={ name }
               year={ year }
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import Title from './Title';
 import planets from '../data/planets';
 import PlanetCard from './PlanetCard';
+import styles from './SolarSystem.module.css';
 
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system">
-        <Title headline="Planetas" />
-        { planets.map(({ name, image }) => (
-          <li key={ name }>
-            <PlanetCard
-              planetName={ name }
-              planetImage={ image }
-            />
-          </li>
-        ))}
+      <div data-testid="solar-system" className={ styles.solarSystem }>
+        <ul className={ styles.listaPlanetas }>
+          {planets.map(({ name, image }) => (
+            <li key={ name } id={ name }>
+              <PlanetCard
+                planetName={ name }
+                planetImage={ image }
+              />
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
